77483a191fb2315a96219f24a3d1e2ac30c1284b
Szymon Gąsior <szgasior@outlook.com>
2019-09-09 19:39:09 +0200

    Initial commit

---
+++ src/main/java/pl/miningportal/MiningportalApplication.java
@@ -0,0 +1,13 @@
+package pl.miningportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiningportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiningportalApplication.class, args);
+    }
+
+}
